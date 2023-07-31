40ccf75e9e1d017e8d7a4cb68e3d91de9ab5c8d7
lane-andreas <134257045+lane-andreas@users.noreply.github.com>
2023-07-31 14:35:10 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,57 @@
+document.addEventListener("DOMContentLoaded", function () {
+  var cursor = document.createElement("div");
+  cursor.className = "custom-cursor";
+  document.body.appendChild(cursor);
+
+  document.addEventListener("mousemove", function (e) {
+    var x = e.clientX;
+    var y = e.clientY;
+    cursor.style.left = x + "px";
+    cursor.style.top = y + "px";
+  });
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+  var cursor = document.createElement("div");
+  cursor.className = "custom-cursor-2";
+  document.body.appendChild(cursor);
+
+  var mouseX = 0;
+  var mouseY = 0;
+  var cursorX = 0;
+  var cursorY = 0;
+
+  function updateCursor() {
+    var dx = mouseX - cursorX;
+    var dy = mouseY - cursorY;
+
+    var lagFactor = 0.1;
+
+    cursorX += dx * lagFactor;
+    cursorY += dy * lagFactor;
+
+    cursor.style.left = cursorX + "px";
+    cursor.style.top = cursorY + "px";
+
+    requestAnimationFrame(updateCursor);
+  }
+
+  document.addEventListener("mousemove", function (e) {
+    mouseX = e.clientX;
+    mouseY = e.clientY;
+  });
+
+  updateCursor();
+});
+
+function changeColor() {
+  var root = document.documentElement;
+
+  var currentOther = getComputedStyle(root).getPropertyValue("--the-other-one");
+
+  if (currentOther == "white") {
+    root.style.setProperty("--the-other-one", "black");
+  } else {
+    root.style.setProperty("--the-other-one", "white");
+  }
+}
